9ba5e9873890e60796d3672bcc0b88c10c8338aa
Matt Narrell <mnarrell@gmail.com>
2014-11-13 08:40:44 -0700

    ZSH refurb.  Thanks @jfred.

--- topics/zsh/install.sh
+++ topics/zsh/install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-
 if [ ! -d ~/.antigen ]; then
     git clone git://github.com/zsh-users/antigen.git ~/.antigen
 fi
--- topics/zsh/prompt.zsh
+++
@@ -1,8 +0,0 @@
-setopt promptsubst
-
-# zgitinit and prompt_mn_setup must be somewhere in your $fpath, see README for more.
-
-# Load the prompt theme system
-autoload -U promptinit
-promptinit
-prompt mn
