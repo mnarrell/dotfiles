1c0e0680fb273f5f0b51c052234a21a5b0d0523e
Matt Narrell <mnarrell@gmail.com>
2016-01-09 20:48:51 -0700

    Scripts Homebrew packages

---
+++ extras/brew.sh
@@ -0,0 +1,36 @@
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew update
+
+brew install ansible
+brew install autoconf
+brew install boost
+brew install coreutils
+brew install git
+brew install gradle
+brew install libyaml
+brew install lzo
+brew install mysql
+brew install openssl
+brew install python
+brew install sbt
+brew install sqlite
+brew install vim
+brew install yajl
+brew install zsh
+brew install ant
+brew install autoenv
+brew install brew-cask
+brew install gdbm
+brew install git-extras
+brew install kafka
+brew install liquibase
+brew install lzop
+brew install nmap
+brew install pcre
+brew install readline
+brew install source-highlight
+brew install tree
+brew install wget
+brew install zookeeper
