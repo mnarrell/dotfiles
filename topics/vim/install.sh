02d62bb9f8799e4b7cf7193709032850067add5b
Matt Narrell <mnarrell@gmail.com>
2016-12-20 09:17:48 -0700

    wippy

--- topics/vim/install.sh
+++ topics/vim/install.sh
@@ -3,9 +3,6 @@ set -e
 
 BASEDIR=$(dirname $0)
 
-#mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/autoload
-
 mkdir -p ~/.vim/autoload
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
