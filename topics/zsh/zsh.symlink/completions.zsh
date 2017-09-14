caa202fabc7aae87ea0e1c764045d60ff557ae1a
Matt Narrell <mnarrell@gmail.com>
2017-09-14 14:03:05 -0600

    Adds ZSH remote ssh path completion

--- topics/zsh/zsh.symlink/completions.zsh
+++ topics/zsh/zsh.symlink/completions.zsh
@@ -12,4 +12,7 @@ hosts=(
 )
 zstyle ':completion:*:hosts' hosts $hosts
 
+# Make zsh know about hosts already accessed by SSH
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
 # vi: ft=zsh
