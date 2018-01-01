d8ec78eee983c38eb03bae31e92b595531f60e26
Matt Narrell <mnarrell@gmail.com>
2018-01-01 11:39:33 -0700

    Path understanding

--- topics/zsh/zsh.symlink/aliases.zsh
+++ topics/zsh/zsh.symlink/aliases.zsh
@@ -1,6 +1,50 @@
-# Load generic aliases
-[ -e "${HOME}/.aliases" ] && source "${HOME}/.aliases"
+alias ls="ls -h"
+alias la="ls -lah"
+alias lsd="ls -l | grep '^d'"
+alias ll="ls -lh"
+alias la="ls -lah"
 
+alias hosts='sudo -E ${EDITOR} /etc/hosts'
+alias sshconfig='${EDITOR} ~/.ssh/config'
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print ${1}'"
+alias flush="dscacheutil -flushcache"
+
+alias clear="echo 'NO!'"
+alias please='sudo $(fc -nl -1)'
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+alias sudo="sudo -E"
+
+alias git='hub'
+alias tig="tig --all"
+alias tis='\tig status'
+
+alias t="tmux attach || tmux"
+alias tk="tmux kill-server"
+
+alias dots="cd ${DOTFILES}"
+alias e="${EDITOR} ."
+
+# Synchronization
+alias rsync-copy="rsync -avz --progress -h"
+alias rsync-move="rsync -avz --progress -h --remove-source-files"
+alias rsync-update="rsync -avzu --progress -h"
+alias rsync-synchronize="rsync -avzu --delete --progress -h"
+
+alias showfiles="defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder"
+alias hidefiles="defaults write com.apple.finder AppleShowAllFiles FALSE; killall Finder"
+alias fixow='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"'
+alias sc="find . -name target | xargs rm -r"
+
+if command -v nvim &> /dev/null; then
+  alias vim="nvim"
+fi
+
+alias dc="docker system prune"
 # Need to redefined these aliases at this point as they get trampled by plugins
 alias ls="gls --color=tty --quoting-style=literal"
 alias ll="ls -lh"
@@ -18,13 +62,12 @@ alias -s markdown='open -a "Marked 2.app"'
 alias k='kubectl'
 alias kp='k get pods -o wide'
 alias ks='k get services -o wide'
-# alias ke='k exec PODS -it sh'
+alias ke='k exec PODS -it sh'
 alias kl='k logs -f PODS'
 
 alias -g IMAGES='$(docker images | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1 \":\" \$2}")'
 alias -g CONTAINERS='$(docker ps -a | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
 
-# Like P, global aliases about kubernetes resources
 alias -g PODS='$(  kubectl get pods  | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
 alias -g DEPLOY='$(kubectl get deploy| fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
 alias -g RS='$(    kubectl get rs    | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
--- topics/zsh/zsh.symlink/prompt.zsh
+++ topics/zsh/zsh.symlink/prompt.zsh
@@ -1,13 +1,21 @@
+autoload -U colors
+colors
+
 autoload -Uz vcs_info
-zstyle ':vcs_info:*' enable git svn
-zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr "%F{red}!%f"
-zstyle ':vcs_info:*' stagedstr "%F{green}+%f"
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' stagedstr "%F{green}●%f" # default 'S'
+zstyle ':vcs_info:*' unstagedstr "%F{red}●%f" # default 'U'
 zstyle ':vcs_info:*' formats " (%b%f%c%u)"
+# zstyle ':vcs_info:git*:*' formats '[%b%m%c%u] ' # default ' (%s)-[%b]%c%u-'
+zstyle ':vcs_info:git*:*' actionformats '[%b|%a%m%c%u] ' # default ' (%s)-[%b|%a]%c%u-'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind
 
-precmd() {
-  vcs_info
+function +vi-git-untracked() {
+  emulate -L zsh
+  if [[ -n $(git ls-files --exclude-standard --others 2> /dev/null) ]]; then
+    hook_com[unstaged]+="%F{blue}●%f"
+  fi
 }
 
 function +vi-git-aheadbehind() {
@@ -20,28 +28,19 @@ function +vi-git-aheadbehind() {
   behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l | tr -d ' ')
   (( $behind )) && gitstatus+=( " %F{red}-${behind}%f" )
 
-  hook_com[branch]="%F{blue}${hook_com[branch]}%f${gitstatus}"
+  hook_com[branch]="%F{blue}${hook_com[branch]}%f${gitstatus} | "
 }
-
-function +vi-git-untracked() {
-  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
-    git status --porcelain | grep '??' &> /dev/null ; then
-    hook_com[unstaged]+='%F{215}?%f'
-  fi
-}
-
 
 # Updates editor information when the keymap changes.
 function zle-keymap-select() {
   zle reset-prompt
   zle -R
 }
-
 zle -N zle-keymap-select
-zle -N edit-command-line
 
 # allow v to edit the command line (standard behaviour)
 autoload -Uz edit-command-line
+zle -N edit-command-line
 bindkey -M vicmd 'v' edit-command-line
 
 function vi_mode_prompt_info() {
@@ -58,9 +57,12 @@ function kube_context_info() {
 }
 
 setopt prompt_subst
+# export PS1="%F{green}${SSH_TTY:+%n@%m}%f%B${SSH_TTY:+:}%b%F{blue}%1~%F{yellow}%B%(1j.*.)%(?..!)%b%f%F{red}%B${SUFFIX}%b%f "
 PROMPT="%F{green}%m%f:%F{white}%2c%f\${vcs_info_msg_0_} %F{yellow}\$(vi_mode_prompt_info)$%f "
 
-RPROMPT="%F{blue}\$(kube_context_info)"
+# RPROMPT="%F{blue}\$(kube_context_info)"
+
+add-zsh-hook precmd vcs_info
 
 # vi: ft=zsh
 
