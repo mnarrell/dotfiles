d9777a63170c95841396057e25b570993a9f6009
Matt Narrell <mnarrell@gmail.com>
2018-01-01 18:05:30 -0700

    Adds some OSX functions to ZSH

--- topics/zsh/zsh.symlink/functions.zsh
+++ topics/zsh/zsh.symlink/functions.zsh
@@ -51,6 +51,39 @@ function fg-bg() {
 zle -N fg-bg
 bindkey '^Z' fg-bg
 
+# Return the path of the frontmost Finder window
+function pfd() {
+  osascript 2>/dev/null <<EOF
+    tell application "Finder"
+      return POSIX path of (target of window 1 as alias)
+    end tell
+EOF
+}
+
+# Return the current Finder selection
+function pfs() {
+  osascript 2>/dev/null <<EOF
+    set output to ""
+    tell application "Finder" to set the_selection to selection
+    set item_count to count the_selection
+    repeat with item_index from 1 to count the_selection
+      if item_index is less than item_count then set the_delimiter to "\n"
+      if item_index is item_count then set the_delimiter to ""
+      set output to output & ((item item_index of the_selection as alias)'s POSIX path) & the_delimiter
+    end repeat
+EOF
+}
+
+# cd to the current Finder directory
+function cdf() {
+  cd "$(pfd)"
+}
+
+# Open a specified man page in Preview app
+function man-preview() {
+  man -t "$@" | open -f -a Preview
+}
+
 # function set-tab-and-window-title() {
 #   emulate -L zsh
 #   local CMD="${1:gs/$/\\$}"
