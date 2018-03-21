5c7f8ce71f6a5c1742b4764c572d051c9485ef4b
Matt Narrell <mnarrell@gmail.com>
2018-03-20 22:13:12 -0600

    tired

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -10,8 +10,11 @@ fi
 alias k='kubectl'
 alias kp='k get pods -o wide'
 alias ks='k get services -o wide'
-alias ke='k exec PODS -it sh'
+alias ke='k exec -it PODS sh'
 alias kl='k logs -f PODS'
+alias kdp='k describe pod PODS'
+alias kds='k describe service SVC'
+alias kcv='k config view | vim -c "set ft=yaml" -'
 
 alias -g PODS='$(  kubectl get pods  | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
 alias -g DEPLOY='$(kubectl get deploy| fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
