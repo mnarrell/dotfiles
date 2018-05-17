7197f79a375b21e160de79f0e8a0af51848112e9
Matt Narrell <mnarrell@gmail.com>
2018-05-17 14:01:34 -0600

    KIP

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -21,3 +21,17 @@ alias -g DEPLOY='$(kubectl get deploy| fzf-tmux --header-lines=1 --reverse --mul
 alias -g RS='$(    kubectl get rs    | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
 alias -g SVC='$(   kubectl get svc   | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
 alias -g ING='$(   kubectl get ing   | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
+
+function kip() {
+  NODE_IP=$(kubectl cluster-info | grep master | egrep -o '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+  NODE_PORTS=$(kubectl get svc -o json SVC | jq -r '.spec.ports[] | "\(.name) \(.nodePort)"')
+  if [[ $(echo "${NODE_PORTS}" | wc -l) -eq 1 ]]; then
+    echo "${NODE_PORTS}" \
+      | awk -v ip="${NODE_IP}" '{print "http://" ip ":"$2}' > >(cat) > >(pbcopy)
+  else
+    echo "${NODE_PORTS}" \
+      | awk -v OFS='\t' -v ip="${NODE_IP}" '{print $1, " http://" ip ":"$2}' \
+      | fzf-tmux --cycle \
+      | awk '{print $2}' > >(cat) > >(pbcopy)
+  fi
+}
