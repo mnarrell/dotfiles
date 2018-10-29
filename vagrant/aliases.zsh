2d651ba64c2c9d51a80c35e01cb0cc4617ec82dc
Matt Narrell <mnarrell@gmail.com>
2018-10-29 09:13:59 -0600

    Vagrant and Ansible

---
+++ vagrant/aliases.zsh
@@ -0,0 +1,6 @@
+alias vu='vagrant up'
+alias vh='vagrant halt'
+alias vp='vagrant provision'
+alias vs='vagrant status'
+alias vd='vagrant destroy -f'
+alias ve='vagrant destroy -f && rm -rf ./.vagrant'
