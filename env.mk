99cdc44a502d4f2f96bf080213d6e0f08b36abd0
Matt Narrell <mnarrell@gmail.com>
2019-10-22 12:08:22 -0600

    Makefile tweaks (again)

--- env.mk
+++ env.mk
@@ -2,3 +2,10 @@ XDG_CONFIG_HOME := ${HOME}/.config
 XDG_CACHE_HOME := ${HOME}/.cache
 XDG_DATA_HOME := ${HOME}/.local/share
 XDG_RUNTIME_DIR := ${HOME}/tmp/xdg_runtime
+
+.PHONY: .ensure.xdg
+.ensure.xdg:
+	mkdir -p $(XDG_CONFIG_HOME)
+	mkdir -p $(XDG_CACHE_HOME)
+	mkdir -p $(XDG_DATA_HOME)
+	mkdir -p $(XDG_RUNTIME_DIR)
