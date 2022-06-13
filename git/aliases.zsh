404cb656bb95b0dcf674b7bf069397512dacbd00
Matt Narrell <mnarrell@gmail.com>
2022-06-12 21:47:19 -0600

    More wip

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,28 +1,23 @@
-#! /usr/bin/env zsh
+#! /usr/bin/env bash
 
 # Tig
 
-alias g="git"
 alias tig="tig --all"
 alias tis='\tig status'
 
 alias pr='gh pr view --web'
 alias rp='gh repo view --web'
 
-# if [ $commands[hub] ]; then
-#   alias git='hub'
-# fi
-
 function git_personal_user() {
-  git config user.email $MY_EMAIL
-  git config user.name $MY_NAME
+  git config user.email "$MY_EMAIL"
+  git config user.name "$MY_NAME"
 }
 
 function eradicate_submodule!(){
-  git submodule deinit -f $1
-  git rm $1
+  git submodule deinit -f "$1"
+  git rm "$1"
   git commit -am "Removed submodule: $1"
-  rm -rf .git/modules/$1
+  rm -rf ".git/modules/$1"
 }
 
 function git_porcelain_submodules() {
