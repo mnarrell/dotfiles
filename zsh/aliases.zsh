91609a4b7e8d9e1da8ff4f13e800cd8662eb59e6
Matt Narrell <mnarrell@gmail.com>
2024-03-09 08:55:07 -0700

    chore: housekeeping

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -54,5 +54,4 @@ alias ./...="nocorrect ./..."
 if [ "$(uname -s)" = "Darwin" ]; then
   alias showfiles="defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder"
   alias hidefiles="defaults write com.apple.finder AppleShowAllFiles FALSE; killall Finder"
-  alias fixow='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"'
 fi
