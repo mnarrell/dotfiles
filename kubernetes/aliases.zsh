1903bcb132527f55182eeff87c346c84a509cc27
Matt Narrell <mnarrell@gmail.com>
2025-04-09 16:08:01 -0600

    housecleaning

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,19 +1,5 @@
 #! /usr/bin/env zsh
 
-if [ $+commands[kubectl] ]; then
-  function kubectl() {
-    source <(command kubectl completion zsh)
-    command kubectl "$@"
-  }
-fi
-
-if [ $+commands[k3d] ]; then
-  function k3d() {
-    source <(command k3d completion zsh)
-    command k3d "$@"
-  }
-fi
-
 alias -g k='kubectl'
 alias -g kp='k get pods -o wide'
 alias -g ks='k get services -o wide'
@@ -24,7 +10,6 @@ alias -g kdp='k describe pod PODS'
 alias -g kds='k describe service SVC'
 alias -g kc='kubectx'
 alias -g kn='kubens'
-alias -g kfixcomp='source <(kubectl completion zsh)'
 alias -g kge='k get events --sort-by='\''.metadata.creationTimestamp'\'' '
 
 alias -g PODS='$(kfuzz pod)'
@@ -38,14 +23,6 @@ alias -g SA='$(kfuzz serviceaccounts)'
 # Fuzzy match a Kubernetes resource
 function kfuzz() {
   kubectl get "$1" | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}"
-}
-
-# Fuzzy port-forwarding
-function kport() {
-  local pod=$(kfuzz pod)
-  local port=$(kubectl get pod "$pod" -o json | jq '.spec.containers | .[0].ports | .[0].containerPort')
-  echo "Forwarding traffic from localhost:${port} to ${pod}:${port}"
-  kubectl port-forward "$pod" "$port:$port" | bat -l log
 }
 
 function ka() {
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,5 +7,3 @@ export HISTFILE="${XDG_DATA_HOME}"/zsh/history
 export HISTSIZE=10000
 export SAVEHIST=${HISTSIZE}
 export HISTDUP=erase
-
-export BOOTSTRAPPED=true
