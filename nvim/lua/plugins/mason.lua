69b08d5abcffa761f04a796885d46a33956249a7
Matt Narrell <mnarrell@gmail.com>
2025-06-04 08:38:00 -0600

    Mason to install lsps

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,42 @@
+local signs = require("icons").signs
+
+return {
+  {
+    "mason-org/mason.nvim",
+    build = ":MasonUpdate",
+    cmd = {
+      "Mason",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+    },
+    opts = {
+      ui = {
+        icons = {
+          package_installed = signs.PassCheck,
+          package_pending = signs.Running,
+          package_uninstalled = signs.GitRemoved,
+        },
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "ansible-language-server",
+        "basedpyright",
+        "bash-language-server",
+        "dockerfile-language-server",
+        "gopls",
+        "helm-ls",
+        "json-lsp",
+        "lua-language-server",
+        "terraform-ls",
+        "tflint",
+        "yaml-language-server",
+      },
+    },
+  },
+}
