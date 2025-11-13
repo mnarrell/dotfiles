9fba83212da5cd4f60c95744962f3570b4eb5a8f
Matt Narrell <mnarrell@gmail.com>
2025-11-13 10:28:31 -0700

    chore: Golang version bump

--- golang/env.zsh
+++ golang/env.zsh
@@ -1,5 +1,5 @@
 #! /usr/bin/env zsh
 
 # export GOBIN=${HOME}/.local/lib/go/bin
-export GOBIN=${ASDF_DATA_DIR}/installs/golang/1.25.3/bin
+export GOBIN=${ASDF_DATA_DIR}/installs/golang/1.25.4/bin
 export GOPATH=${XDG_DATA_HOME}/go
