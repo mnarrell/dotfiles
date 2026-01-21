d3a396e9f36635c4151047c3cfb3f22ae2808ea3
Matt Narrell <mnarrell@gmail.com>
2026-01-21 11:23:52 -0700

    feat: WIP

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,4 +1,3 @@
-local diagnostic_icons = require("icons").diagnostics
 local methods = vim.lsp.protocol.Methods
 local auGroup = vim.api.nvim_create_augroup("lsp", { clear = true })
 
@@ -11,17 +10,6 @@ local on_attach = function(client, bufnr)
   local map = function(lhs, rhs)
     vim.keymap.set("n", lhs, rhs, { buffer = bufnr, silent = true })
   end
-
-  -- vim.lsp.inlay_hint.enable(false)
-
-  -- map("<c-]>", vim.lsp.buf.definition)
-  -- map("gD", vim.lsp.buf.declaration)
-  -- map("<Leader>D", vim.lsp.buf.type_definition)
-  -- map("gf", require("telescope.builtin").lsp_references)
-  -- map("grr", require("telescope.builtin").lsp_references)
-  -- map("gri", require("telescope.builtin").lsp_implementations)
-  -- map("<LocalLeader>s", require("telescope.builtin").lsp_document_symbols)
-  -- map("<LocalLeader>w", require("telescope.builtin").lsp_dynamic_workspace_symbols)
 
   -- stylua: ignore start
   map("<c-]>", function() Snacks.picker.lsp_definitions({jump = { tagstack = true, reuse_win = false }}) end)
@@ -38,29 +26,12 @@ local on_attach = function(client, bufnr)
   if client:supports_method(methods.textDocument_definition) then
     map("de", function()
       Snacks.picker.lsp_definitions({ confirm = "vsplit", jump = { tagstack = true, reuse_win = false } })
-      -- require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" })
     end)
   end
 
   vim.api.nvim_create_user_command("TD", function()
     vim.diagnostic.enable(not vim.diagnostic.is_enabled())
   end, {})
-
-  -- if client:supports_method(methods.textDocument_documentHighlight) then
-  --   local under_cursor_hl_group = vim.api.nvim_create_augroup("mn/cursor_highlights", { clear = false })
-  --   vim.api.nvim_create_autocmd({ "CursorHold", "InsertLeave" }, {
-  --     group = under_cursor_hl_group,
-  --     desc = "Highlight references under cursor",
-  --     buffer = bufnr,
-  --     callback = vim.lsp.buf.document_highlight,
-  --   })
-  --   vim.api.nvim_create_autocmd({ "CursorMoved", "InsertEnter", "BufLeave" }, {
-  --     group = under_cursor_hl_group,
-  --     desc = "Clear highlight references",
-  --     buffer = bufnr,
-  --     callback = vim.lsp.buf.clear_references,
-  --   })
-  -- end
 
   if client:supports_method(methods.textDocument_inlayHint) then
     vim.api.nvim_create_user_command("TH", function()
@@ -70,7 +41,6 @@ local on_attach = function(client, bufnr)
 
   if client:supports_method("textDocument/foldingRange") then
     local win = vim.api.nvim_get_current_win()
-    -- vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
     vim.wo[win].foldexpr = "v:lua.vim.lsp.foldexpr()"
   end
 end
@@ -96,6 +66,7 @@ vim.lsp.buf.signature_help = function()
 end
 
 -- Diagnostics.
+local diagnostic_icons = require("icons").diagnostics
 vim.diagnostic.config({
   virtual_text = false,
   update_in_insert = false,
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,96 +1,48 @@
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  callback = function(event)
+    local ok, ts = pcall(require, "nvim-treesitter")
+    if not ok then
+      return
+    end
+
+    local parsers = require("nvim-treesitter.parsers")
+    if not parsers[event.match] or not ts.install then
+      return
+    end
+
+    local ft = vim.bo[event.buf].ft
+    local lang = vim.treesitter.language.get_lang(ft)
+    ts.install({ lang }):await(function(err)
+      if err then
+        vim.notify("Treesitter install error for ft: " .. ft .. "err:" .. err)
+        return
+      end
+
+      pcall(vim.treesitter.start, event.buf)
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    end)
+  end,
+})
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate",
-  dependencies = {
-    "HiPhish/rainbow-delimiters.nvim",
-    -- { "windwp/nvim-ts-autotag", config = true },
-  },
-  opts = {
-    ensure_installed = {
-      "bash",
-      "comment",
-      "diff",
-      "dockerfile",
-      "elixir",
-      "gitignore",
-      "go",
-      "gomod",
-      "gowork",
-      "hcl",
-      "http",
-      "java",
-      "json",
-      "jsonc",
-      "lua",
-      "make",
-      "python",
-      "regex",
-      "ruby",
-      "scala",
-      "terraform",
-      "toml",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
-    },
-    sync_install = false,
+  event = "VeryLazy",
+  branch = "main",
+  build = function()
+    if vim.fn.exists(":TSUpdate") == 2 then
+      vim.cmd("TSUpdate")
+    end
+  end,
 
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
+  config = function(_, _)
+    local ensure_installed = {}
 
-    indent = {
-      enable = true,
-      disable = { "yaml", "go" },
-    },
+    local ok, ts = pcall(require, "nvim-treesitter")
+    if not ok then
+      return
+    end
 
-    -- custom_captures = {
-    --   ["comment"] = "Comment",
-    -- },
-
-    -- rainbow = {
-    --   enable = true,
-    --   extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    --   max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- },
-
-    -- context_commentstring = { enable = true },
-
-    -- textobjects = {
-    --   move = {
-    --     enable = true,
-    --     set_jumps = true, -- whether to set jumps in the jumplist
-    --     goto_next_start = {
-    --       ["]]"] = "@function.outer",
-    --     },
-    --     goto_previous_start = {
-    --       ["[["] = "@function.outer",
-    --     },
-    --   },
-    --   select = {
-    --     enable = true,
-    --     lookahead = true,
-    --     keymaps = {
-    --       --       -- ["af"] = "@function.outer",
-    --       --       -- ["if"] = "@function.inner",
-    --       --       -- ["ac"] = "@class.outer",
-    --       --       -- ["ic"] = "@class.inner",
-    --       --       -- ["ab"] = "@block.outer",
-    --       --       -- ["ib"] = "@block.inner",
-    --       ["aa"] = "@parameter.outer",
-    --       ["ia"] = "@parameter.inner",
-    --     },
-    --   },
-    -- },
-
-    -- autopairs = { enable = true },
-    -- autotag = { enable = true },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+    ts.install(ensure_installed)
   end,
 }
