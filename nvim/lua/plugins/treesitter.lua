f97d987c4ef40dc877fa13f0669509d062a4501c
Matt Narrell <mnarrell@gmail.com>
2026-01-28 10:18:24 -0700

    feat: WIP

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,48 +1,47 @@
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  callback = function(event)
-    local ok, ts = pcall(require, "nvim-treesitter")
-    if not ok then
-      return
-    end
-
-    local parsers = require("nvim-treesitter.parsers")
-    if not parsers[event.match] or not ts.install then
-      return
-    end
-
-    local ft = vim.bo[event.buf].ft
-    local lang = vim.treesitter.language.get_lang(ft)
-    ts.install({ lang }):await(function(err)
-      if err then
-        vim.notify("Treesitter install error for ft: " .. ft .. "err:" .. err)
-        return
-      end
-
-      pcall(vim.treesitter.start, event.buf)
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    end)
-  end,
-})
-
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = "VeryLazy",
-  branch = "main",
-  build = function()
-    if vim.fn.exists(":TSUpdate") == 2 then
-      vim.cmd("TSUpdate")
-    end
-  end,
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local parsers = {
+      "bash",
+      "comment",
+      "diff",
+      "dockerfile",
+      "elixir",
+      "gitignore",
+      "go",
+      "gomod",
+      "gowork",
+      "hcl",
+      "http",
+      "java",
+      "json",
+      "jsonc",
+      "lua",
+      "make",
+      "python",
+      "regex",
+      "ruby",
+      "scala",
+      "terraform",
+      "toml",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    }
 
-  config = function(_, _)
-    local ensure_installed = {}
+    local ts = require("nvim-treesitter")
+    ts.setup()
+    ts.install(parsers)
 
-    local ok, ts = pcall(require, "nvim-treesitter")
-    if not ok then
-      return
-    end
-
-    ts.install(ensure_installed)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
   end,
 }
